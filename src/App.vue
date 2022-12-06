2f475b6cdcbcd047696894d958859e51edf5cba0
Fabien Grignoux <fabien.grignoux@outlook.com>
2022-12-06 17:09:40 +0100

    App initialisation

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+const name = "Fabien";
+</script>
+
+<template>
+  <v-app>
+    <v-app-bar color="primary" light>
+      <v-app-bar-title> DevWall </v-app-bar-title>
+      <v-spacer />
+      <v-btn>Menu 1</v-btn>
+      <v-btn>Menu 2</v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <h1>Hello {{ name }} !</h1>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
